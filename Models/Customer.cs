8dbf7b9055344c3fcdef429884f9cdcfd0bccea7
Phong <phamkhacthanhphong@gmail.con>
2022-06-23 22:45:39 +0700

    phong

---
+++ Models/Customer.cs
@@ -0,0 +1,37 @@
+﻿using FPTLibrary.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FPTBook.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "This field cannot be empty!")]
+        [MinLength(2, ErrorMessage = "The name length must be longer than 2!")]
+        [MaxLength(50, ErrorMessage = "The name length exceed maximum required characters!")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "This field cannot be empty!")]
+        [MinLength(2, ErrorMessage = "The name length must be longer than 2!")]
+        [MaxLength(50, ErrorMessage = "The name length exceed maximum required characters!")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "This field cannot be empty!")]
+        [Url]
+        public string Image { get; set; }
+        [Required(ErrorMessage = "This field cannot be empty!")]
+        [MinLength(5, ErrorMessage = "Email must be at least 5 characters long!")]
+        [MaxLength(200, ErrorMessage = "Email cannot exceed 200 characters length!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "This field cannot be empty!")]
+        [MinLength(10, ErrorMessage = "Phone number must be at least 10 characters long!")]
+        [MaxLength(13, ErrorMessage = "Phone number length cannot exceed 13 characters!")]
+        public string Mobile { get; set; }
+        [Required(ErrorMessage = "This field cannot be empty!")]
+        [MinLength(3, ErrorMessage = "Address must be at least 3 characters long!")]
+        [MaxLength(200, ErrorMessage = "Address length cannot exceed 200 characters!")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "This field cannot be empty!")]
+        public string Password { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace FPTLibrary.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+    }
+}
